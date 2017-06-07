881ef1202b4610b01ea07ab430b3d42e970ecee1
tp <tp@octavore.com>
2017-06-07 08:42:46 -0700

    Remove unused js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 let gulp = require('gulp');
 
-require('./gulp/js')
 require('./gulp/css')
 require('./gulp/html')
 
-gulp.task('default', ['js:webpack', 'html', 'css', 'images'])
+gulp.task('default', ['html', 'css', 'images'])
